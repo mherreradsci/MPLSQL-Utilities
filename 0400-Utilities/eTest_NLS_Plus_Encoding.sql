0fb87359470d614a42ba54687258f6b41209adbc
mherreradsci <marcelo.herrerat@gmail.com>
2025-03-23 02:05:47 -0300

    Improve TIUTIL_OWN_Build_All.sql and fix test fro NLS support

--- 0400-Utilities/eTest_NLS_Plus_Encoding.sql
+++ 0400-Utilities/eTest_NLS_Plus_Encoding.sql
@@ -3,19 +3,19 @@
 --$ NLS_LANG=SPANISH_CHILE.WE8MSWIN1252 sqlplus system@centos-ora11g @eTest_NLS_Plus_Encoding.sql < /tmp/pass
 --$ NLS_LANG=SPANISH_CHILE.UTF8 sqlplus system@centos-ora11g @eTest_NLS_Plus_Encoding.sql < /tmp/pass
 
+--$ NLS_LANG=SPANISH_CHILE.UTF8 sqlplus system@docker-19c-pdb1-testing @eTest_NLS_Plus_Encoding.sql
+
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
 set echo off lines 600 pagesize 9999
-col DB_CHARACTERSET format a30
 
-select VALUE as DB_CHARACTERSET from nls_database_parameters where parameter='NLS_CHARACTERSET';
 
 col PARAMETER format a30 wrap
 col VALUE format a30 wrap
 
-SELECT * FROM nls_session_parameters where parameter in('NLS_TERRITORY', 'NLS_LANGUAGE','NLS_NUMERIC_CHARACTERS','NLS_DATE_FORMAT','NLS_DATE_LANGUAGE');
+select * from nls_database_parameters where parameter like 'NLS_%CHARACTERSET' order by parameter;
 
-select 'ñ' as ch, chr(241) from dual;
+SELECT * FROM nls_session_parameters where parameter in('NLS_TERRITORY', 'NLS_LANGUAGE','NLS_NUMERIC_CHARACTERS','NLS_DATE_FORMAT','NLS_DATE_LANGUAGE');
 
 
 -- Valida que el separador decimal sea coma (,)
@@ -29,8 +29,12 @@ select case to_char(next_day(trunc(sysdate, 'month'), 'lunes'), 'd') when '1' th
  
 col C_ENCODING new_value V_ENCODING
 
+-- Valida que el lenguaje y character set sea el correcto
+select ascii('ñ') as a, chr(ascii('ñ')) as c, unistr('\00f1') as u from dual;
+
 with encoding as (
-    select case when 'ñ' != chr(241) then 'Fail' else 'Pass: Encoding Ok' end as TestEncoding from dual
+    select case when 'ñ' != chr(ascii('ñ')) then 'Fail' else 'Pass: Encoding Ok' end as TestEncoding from dual
 )     
 select  case testencoding when 'Pass: Encoding Ok' then 0 else 1/0 end as force_error  
 from encoding;
+
